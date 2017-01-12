97272e014f487f3e4f765023473c2c1a822e479c
vagrant <vagrant@homestead>
2017-01-12 02:35:02 +0000

    finish static page

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title','Sample App')- Laravel Basic Tutorial</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+  <h1>home</h1>
+@stop
